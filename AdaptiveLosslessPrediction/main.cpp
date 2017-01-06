61b4ead8c63d0132d29eb8efc3ca001bfc9ce32b
Mo DeJong <mo@modejong.com>
2017-01-06 15:21:30 -0800

    init empty Xcode command line project

---
+++ AdaptiveLosslessPrediction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AdaptiveLosslessPrediction
+//
+//  Created by Mo DeJong on 1/6/17.
+//  Copyright © 2017 HelpURock. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
